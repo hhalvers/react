9a49376ae96a641ae54f4902214f24597d0084fe
Heath Halverson <hhalver@me.com>
2020-10-19 11:21:17 -0700

    presentational components

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -12,12 +12,12 @@ class CampsiteInfo extends Component {
             return (
                 <div className="col-md-5 md-1">
                     <h4>Comments</h4>
-                    {comments.map(comment => <div key={comment.id}> 
-                                            <p>{comment.text}</p> 
-                                            <p>{comment.author} - {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p></div>)}
+                    {comments.map(comment => <div key={comment.id}>
+                        <p>{comment.text}</p>
+                        <p>{comment.author} - {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comment.date)))}</p></div>)}
                 </div>
-            ) 
- 
+            )
+
         }
         return <div />;
 
@@ -42,12 +42,12 @@ class CampsiteInfo extends Component {
     render() {
         if (this.props.campsite) {
             return (
-
-                <div className="row">
-                    {this.renderCampsite(this.props.campsite)}
-                    {this.renderComments(this.props.campsite.comments)}
+                <div className="container">
+                    <div className="row">
+                        {this.renderCampsite(this.props.campsite)}
+                        {this.renderComments(this.props.campsite.comments)}
+                    </div>
                 </div>
-
             );
         }
         else {
