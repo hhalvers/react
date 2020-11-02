b079b3e303538628b7e8b0e04471c3b73710a984
Heath Halverson <hhalver@me.com>
2020-11-02 12:42:49 -0800

    thunk

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -3,7 +3,7 @@ import { Modal, ModalHeader, ModalBody, Label, Card, CardImg, CardText, CardBody
 import { Link } from 'react-router-dom';
 import { Button } from 'reactstrap';
 import { Control, LocalForm, Errors } from 'react-redux-form';
-
+import { Loading } from './LoadingComponent';
 
 const required = val => val && val.length > 0;
 const maxLength = len => val => !val || (val.length <= len);
@@ -149,6 +149,26 @@ function RenderCampsite({ campsite }) {
 }
 
 function CampsiteInfo(props) {
+    if (props.isLoading) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <Loading />
+                </div>
+            </div>
+        );
+    }
+    if (props.errMess) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col">
+                        <h4>{props.errMess}</h4>
+                    </div>
+                </div>
+            </div>
+        );
+    }
     if (props.campsite) {
         return (
             <div className="container">
--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,10 +1,12 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 
 import { Campsites } from './campsites';
 import { Comments } from './comments';
 import { Partners } from './partners';
 import { Promotions } from './promotions';
 
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 
 export const ConfigureStore = () => {
     const store = createStore(
@@ -13,7 +15,9 @@ export const ConfigureStore = () => {
             comments: Comments,
             partners: Partners,
             promotions: Promotions
-        })
+        }),
+        applyMiddleware(thunk, logger)
+
     );
 
     return store;
