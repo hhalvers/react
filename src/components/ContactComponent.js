a583f5485cb599c2b8861e4de59410df882ad132
Heath Halverson <hhalver@me.com>
2020-11-02 12:53:03 -0800

    react redux forms revisited

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import { Breadcrumb, Modal, BreadcrumbItem, Button, Label, Col, Row } from 'reactstrap';
 import { Link } from 'react-router-dom';
-import { Control, LocalForm, Errors } from 'react-redux-form';
+import { Control, LocalForm, Errors, Form } from 'react-redux-form';
+
 
 const required = val => val && val.length > 0;
 const maxLength = len => val => !val || (val.length <= len);
@@ -37,8 +38,9 @@ class Contact extends Component {
 
 
     handleSubmit(values) {
-        console.log('Current state is: ' + JSON.stringify(values));
-        alert('Current state is: ' + JSON.stringify(values));
+        console.log('Current State is: ' + JSON.stringify(values));
+        alert('Current State is: ' + JSON.stringify(values));
+        this.props.resetFeedbackForm();
     }
 
     render() {
@@ -76,7 +78,7 @@ class Contact extends Component {
                         <hr />
                     </div>
                     <div className="col-md-10">
-                        <LocalForm onSubmit={values => this.handleSubmit(values)}>
+                        <Form model="feedbackForm" onSubmit={values => this.handleSubmit(values)}>
                             <Row className="form-group" >
                                 <Label htmlFor="firstName" md={2}>First Name</Label>
                                 <Col md={10}>
@@ -215,7 +217,7 @@ class Contact extends Component {
                                     </Button>
                                 </Col>
                             </Row>
-                        </LocalForm>
+                        </Form>
                     </div>
                 </div>
             </div>
--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -8,13 +8,19 @@ import { Promotions } from './promotions';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 
+import { createForms } from 'react-redux-form';
+import { InitialFeedback } from './forms';
+
 export const ConfigureStore = () => {
     const store = createStore(
         combineReducers({
             campsites: Campsites,
             comments: Comments,
             partners: Partners,
-            promotions: Promotions
+            promotions: Promotions,
+            ...createForms({
+                feedbackForm: InitialFeedback
+            })
         }),
         applyMiddleware(thunk, logger)
 
